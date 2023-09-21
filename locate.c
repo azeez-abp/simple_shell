55f24ba8bce8e8f881585a5ff9e5023ee1b859e8
azeez-abp <adioadeyoriazeez@gmail.com>
2023-09-21 03:45:25 -0700

    sync change

--- locate.c
+++ locate.c
@@ -1,113 +1,140 @@
 #include "shell.h"
 
-alias_t *add_alias_end(alias_t **head, char *name, char *value);
-void free_alias_list(alias_t *head);
-list_t *add_node_end(list_t **head, char *dir);
-void free_list(list_t *head);
+char *fill_path_dir(char *path);
+list_t *get_path_dir(char *path);
 
 /**
- * add_alias_end - Adds a node to the end of a alias_t linked list.
- * @head: A pointer to the head of the list_t list.
- * @name: The name of the new alias to be added.
- * @value: The value of the new alias to be added.
+ * get_location - Locates a command in the PATH.
+ * @command: The command to locate.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ * Return: If an error occurs or the command cannot be located - NULL.
+ *         Otherwise - the full pathname of the command.
  */
-alias_t *add_alias_end(alias_t **head, char *name, char *value)
+char *get_location(char *command)
 {
-	alias_t *new_node = malloc(sizeof(alias_t));
-	alias_t *last;
+	char **path, *temp;
+	list_t *dirs, *head;
+	struct stat st;
 
-	if (!new_node)
+	path = _getenv("PATH");
+	if (!path || !(*path))
 		return (NULL);
 
-	new_node->next = NULL;
-	new_node->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (!new_node->name)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->value = value;
-	_strcpy(new_node->name, name);
+	dirs = get_path_dir(*path + 5);
+	head = dirs;
 
-	if (*head)
+	while (dirs)
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new_node;
-	}
-	else
-		*head = new_node;
+		temp = malloc(_strlen(dirs->dir) + _strlen(command) + 2);
+		if (!temp)
+			return (NULL);
 
-	return (new_node);
+		_strcpy(temp, dirs->dir);
+		_strcat(temp, "/");
+		_strcat(temp, command);
+
+		if (stat(temp, &st) == 0)
+		{
+			free_list(head);
+			return (temp);
+		}
+
+		dirs = dirs->next;
+		free(temp);
+	}
+
+	free_list(head);
+
+	return (NULL);
 }
 
 /**
- * add_node_end - Adds a node to the end of a list_t linked list.
- * @head: A pointer to the head of the list_t list.
- * @dir: The directory path for the new node to contain.
+ * fill_path_dir - Copies path but also replaces leading/sandwiched/trailing
+ *		   colons (:) with current working directory.
+ * @path: The colon-separated list of directories.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new node.
+ * Return: A copy of path with any leading/sandwiched/trailing colons replaced
+ *	   with the current working directory.
  */
-list_t *add_node_end(list_t **head, char *dir)
+char *fill_path_dir(char *path)
 {
-	list_t *new_node = malloc(sizeof(list_t));
-	list_t *last;
+	int i, length = 0;
+	char *path_copy, *pwd;
 
-	if (!new_node)
+	pwd = *(_getenv("PWD")) + 4;
+	for (i = 0; path[i]; i++)
+	{
+		if (path[i] == ':')
+		{
+			if (path[i + 1] == ':' || i == 0 || path[i + 1] == '\0')
+				length += _strlen(pwd) + 1;
+			else
+				length++;
+		}
+		else
+			length++;
+	}
+	path_copy = malloc(sizeof(char) * (length + 1));
+	if (!path_copy)
+		return (NULL);
+	path_copy[0] = '\0';
+	for (i = 0; path[i]; i++)
+	{
+		if (path[i] == ':')
+		{
+			if (i == 0)
+			{
+				_strcat(path_copy, pwd);
+				_strcat(path_copy, ":");
+			}
+			else if (path[i + 1] == ':' || path[i + 1] == '\0')
+			{
+				_strcat(path_copy, ":");
+				_strcat(path_copy, pwd);
+			}
+			else
+				_strcat(path_copy, ":");
+		}
+		else
+		{
+			_strncat(path_copy, &path[i], 1);
+		}
+	}
+	return (path_copy);
+}
+
+/**
+ * get_path_dir - Tokenizes a colon-separated list of
+ *                directories into a list_s linked list.
+ * @path: The colon-separated list of directories.
+ *
+ * Return: A pointer to the initialized linked list.
+ */
+list_t *get_path_dir(char *path)
+{
+	int index;
+	char **dirs, *path_copy;
+	list_t *head = NULL;
+
+	path_copy = fill_path_dir(path);
+	if (!path_copy)
+		return (NULL);
+	dirs = _strtok(path_copy, ":");
+	free(path_copy);
+	if (!dirs)
 		return (NULL);
 
-	new_node->dir = dir;
-	new_node->next = NULL;
-
-	if (*head)
+	for (index = 0; dirs[index]; index++)
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new_node;
+		if (add_node_end(&head, dirs[index]) == NULL)
+		{
+			free_list(head);
+			free(dirs);
+			return (NULL);
+		}
 	}
-	else
-		*head = new_node;
 
-	return (new_node);
-}
+	free(dirs);
 
-/**
- * free_alias_list - Frees a alias_t linked list.
- * @head: THe head of the alias_t list.
- */
-void free_alias_list(alias_t *head)
-{
-	alias_t *next;
-
-	while (head)
-	{
-		next = head->next;
-		free(head->name);
-		free(head->value);
-		free(head);
-		head = next;
-	}
-}
-
-/**
- * free_list - Frees a list_t linked list.
- * @head: The head of the list_t list.
- */
-void free_list(list_t *head)
-{
-	list_t *next;
-
-	while (head)
-	{
-		next = head->next;
-		free(head->dir);
-		free(head);
-		head = next;
-	}
+	return (head);
 }
