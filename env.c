f736ca3d18a6953050dca84c572b4b0f69e3a626
azeez-abp <adioadeyoriazeez@gmail.com>
2023-09-21 02:58:39 -0700

    First ammned all files

--- env.c
+++ env.c
@@ -1,152 +1,75 @@
-#include "header.h"
+#include "shell.h"
+
+char **_copyenv(void);
+void free_env(void);
+char **_getenv(const char *var);
 
 /**
- * setenv - set enviroment variable
- * @key: is the key
- * @value: is the value
+ * _copyenv - Creates a copy of the environment.
  *
+ * Return: If an error occurs - NULL.
+ *         O/w - a double pointer to the new copy.
  */
-int  setenvfunc(char *key, char *val)
+char **_copyenv(void)
 {
-	if (key[0])
+	char **new_environ;
+	size_t size;
+	int index;
+
+	for (size = 0; environ[size]; size++)
+		;
+
+	new_environ = malloc(sizeof(char *) * (size + 1));
+	if (!new_environ)
+		return (NULL);
+
+	for (index = 0; environ[index]; index++)
 	{
-		int haset;
+		new_environ[index] = malloc(_strlen(environ[index]) + 1);
 
-		haset = _setenv(key, val);
-
-		if (haset != 0)
+		if (!new_environ[index])
 		{
-			printf("Error!\n");
+			for (index--; index >= 0; index--)
+				free(new_environ[index]);
+			free(new_environ);
+			return (NULL);
 		}
+		_strcpy(new_environ[index], environ[index]);
 	}
+	new_environ[index] = NULL;
 
-	return (0);
+	return (new_environ);
 }
 
 /**
- * unsetenv - set enviroment variable
- * @key: is the key
- *
+ * free_env - Frees the the environment copy.
  */
-void unsetenvfunc(char *key)
+void free_env(void)
 {
-	if (key[0])
-	{
-		int haset;
+	int index;
 
-		haset = _unsetenv(key);
-
-		if (haset <  0)
-		{
-			printf("Error!\n");
-		}
-	}
+	for (index = 0; environ[index]; index++)
+		free(environ[index]);
+	free(environ);
 }
 
 /**
  * _getenv - Gets an environmental variable from the PATH.
- * @str: The name of the environmental variable to get.
+ * @var: The name of the environmental variable to get.
  *
  * Return: If the environmental variable does not exist - NULL.
  *         Otherwise - a pointer to the environmental variable.
  */
-char **_getenv(char *str)
+char **_getenv(const char *var)
 {
 	int index, len;
 
-	len = _strlen(str);
+	len = _strlen(var);
 	for (index = 0; environ[index]; index++)
 	{
-		if (_strncmp(str, environ[index], len) == 0)
+		if (_strncmp(var, environ[index], len) == 0)
 			return (&environ[index]);
 	}
 
 	return (NULL);
-}
-/**
- * _setenv - Changes or adds an environmental variable to the PATH.
- * @key: string use to identify the env
- * @val: the value of the env
- * Return: 0 success , -1
- */
-int _setenv(char *key, char *val)
-{
-	char **env_var = NULL, **new_environ, *new_value;
-	size_t size;
-	int index;
-
-	if (!key || !val)
-		return (-1);
-
-	new_value = malloc(_strlen(key) + 1 + _strlen(val) + 1);
-	if (!new_value)
-		return (-1);
-	_strcpy(new_value, key);
-	_strcat(new_value, "=");
-	_strcat(new_value, val);
-
-	env_var = _getenv(key);
-	if (env_var)
-	{
-		return (0);
-	}
-	for (size = 0; environ[size]; size++)
-		;
-
-	new_environ = malloc(sizeof(char *) * (size + 2));
-	if (!new_environ)
-	{
-		free(new_value);
-		return  (-1);
-	}
-
-	for (index = 0; environ[index]; index++)
-		new_environ[index] = environ[index];
-
-	/*free(environ);*/
-	environ = new_environ;
-	environ[index] = new_value;
-	environ[index + 1] = NULL;
-	return (0);
-}
-
-/**
- * _unsetenv - Deletes an environmental variable from the PATH.
- * @key: String of key to use in setting env
- * Description: args[1] is the PATH variable to remove
- * Return: If an error occurs - -1.
- *	 Otherwise - 0.
- */
-int _unsetenv(char *key)
-{
-	char **env_var, **new_environ;
-	size_t size;
-	int index, index2;
-
-	if (!key)
-		return (-1);
-	env_var = _getenv(key);
-	if (!env_var)
-		return (0);
-
-	for (size = 0; environ[size]; size++)
-		;
-
-	new_environ = malloc(sizeof(char *) * size);
-	if (!new_environ)
-		return (-1);
-	for (index = 0, index2 = 0; environ[index]; index++)
-	{
-		if (*env_var == environ[index])
-		{
-			free(*env_var);
-			continue;
-		}
-		new_environ[index2] = environ[index];
-		index2++;
-	}
-	environ = new_environ;
-	environ[size - 1] = NULL;
-
-	return (0);
 }
